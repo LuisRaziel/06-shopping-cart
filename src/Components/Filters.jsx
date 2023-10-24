14afefb19d8d13cf8bb12c3a98d23038ed3b3baa
Luis Raziel <razieldiazh@gmail.com>
2023-10-24 14:43:18 -0600

    use useId for create id in the elements

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -1,31 +1,33 @@
-import { useState } from 'react'
+import { useState, useId } from 'react'
 import './Filters.css'
 export function Filters({ onChange }) {
   const [minPrice, setMinPrice] = useState(0)
+  const minPriceFilterId = useId()
+  const categoryFilterId = useId()
 
   const handleChangeMinPrice = (event) => {
     setMinPrice(event.target.value)
-    onChange(prevState => ({
-        ...prevState,
-        minPrice: event.target.value
+    onChange((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
     }))
   }
 
-  // 🔻 no se debería pasar una función de actualización del estado como prop a un hijo 
+  // 🔻 no se debería pasar una función de actualización del estado como prop a un hijo
   const handleChangeCategory = (event) => {
-    onChange(prevState => ({
-        ...prevState,
-        category: event.target.value
+    onChange((prevState) => ({
+      ...prevState,
+      category: event.target.value,
     }))
   }
 
   return (
     <section className='filters'>
       <div>
-        <label htmlFor='price'>Price</label>
+        <label htmlFor={minPriceFilterId}>Price</label>
         <input
           type='range'
-          id='price'
+          id={minPriceFilterId}
           min={0}
           max={1000}
           onChange={handleChangeMinPrice}
@@ -33,8 +35,8 @@ export function Filters({ onChange }) {
         <span>{minPrice}</span>
       </div>
       <div>
-        <label htmlFor='category'>Categoría </label>
-        <select name='' id='category' onChange={handleChangeCategory}>
+        <label htmlFor={categoryFilterId}>Categoría </label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
           <option value='all'>Todas</option>
           <option value='laptops'>Laptops</option>
           <option value='smartphones'>Celulares</option>
